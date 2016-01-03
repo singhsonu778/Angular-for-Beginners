d80b42380087057540a02f13e981abae51b34294
Sonu Kumar Singh <singhsonu778@gmail.com>
2016-01-03 22:37:12 +0530

    Add function to controller

--- myCtrl.js
+++ myCtrl.js
@@ -1,4 +1,7 @@
 app.controller('myCtrl', function($scope) {
 	$scope.firstName = "Sonu";
 	$scope.lastName = "Singh";
+	$scope.fullName = function() {
+        return $scope.firstName + " " + $scope.lastName;
+    };
 });
